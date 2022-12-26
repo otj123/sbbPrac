6c0306294bcee36524aa6ca552adf3a640e3bf97
otj <altest2@daum.net>
2022-12-26 21:50:44 +0900

    sbb 환경셋팅

---
+++ sbb/src/main/java/com/mysite/sbb/HelloController.java
@@ -0,0 +1,15 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "Hello World Devtools를 확인 위한 수정 문구 dfsfdsfdsfdsaa";
+	}
+	
+}
