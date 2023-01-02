c9adaa92038c1516fd98f313a70ff2b49524f6bd
otj <altest2@daum.net>
2023-01-02 21:36:17 +0900

    "서비스 생성"

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,20 +5,33 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/question")
 public class QuestionController {
 	
-	private final QuestionRepository questionRepository;
+	//private final QuestionRepository questionRepository;
+	private final QuestionService questionService;
 	
-	@GetMapping("/question/list")
+	//@GetMapping("/question/list")
+	@GetMapping("/list")
 	/* @ResponseBody */
 	public String list(Model model) {
-		List<Question> questionList = questionRepository.findAll();
+		//List<Question> questionList = questionRepository.findAll();
+		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
 		return "question_list";
+	}
+	//@GetMapping(value = "/question/detail/{id}")
+	@GetMapping(value = "/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question",question);
+		return "question_detail";
 	}
 }
